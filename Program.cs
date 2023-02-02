dd0568bf93459955b300d6e29f6ebbd77b7e9a1f
SYARGEY <lss1895@mail.ru>
2023-02-02 19:43:26 +0300

    Семинар 4 ДЗ 1

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 1: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B. Не использовать Math.Pow
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int InputInt(string massage)
+{
+    System.Console.WriteLine($"{massage} > ");
+    int a = Convert.ToInt32(Console.ReadLine());
+    return a;
+}
+int NumberA = InputInt("Введите число А");
+int NumberB = InputInt("Введите число B");
+
+int Stepen(int NumberA, int NumberB)
+
+{
+    int result = 1;
+    for (int i = 1; i <= NumberB; i++)
+    {
+        result = result * NumberA;
+    }
+    return result;
+}
+System.Console.WriteLine($"Степень равна {Stepen(NumberA,NumberB)}");
